f4235fbde6a9a8e9f66f747cccabec7a60302fa0
estefaniamunoz801-ai <estefania.munoz801@pascualbravo.edu.co>
2025-10-23 09:23:59 -0500

    Se crea el archivo index y se enruta en app

--- flask_app/src/app.py
+++ flask_app/src/app.py
@@ -1,11 +1,15 @@
 import os 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'My first hello world'
+
+@app.route('/temp')
+def welcom():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5000)
